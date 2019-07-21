0feee79826b616ae6afa27e2de709c024a95cd9a
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-07-20 21:42:21 -0500

    transcribe more of the Greskell README.

--- src/Greskell_Examples.hs
+++ src/Greskell_Examples.hs
@@ -7,15 +7,15 @@ module Greskell_Examples where
 --import Test.Hspec
 import Control.Category ((>>>))
 import Control.Exception.Safe (bracket, try, SomeException)
-import Control.Monad (guard)
+-- import Control.Monad (guard)
 import Data.Foldable (toList)
 import Data.Function ((&))
-import Data.Monoid (mempty)
+-- import Data.Monoid (mempty)
 import Data.Text
-import Text.Heredoc (here)
-import qualified Data.Aeson as A
-import qualified Data.Aeson.Types as A
-import qualified Data.HashMap.Strict as HM
+-- import Text.Heredoc (here)
+-- import qualified Data.Aeson as A
+-- import qualified Data.Aeson.Types as A
+-- import qualified Data.HashMap.Strict as HM
 
 import Data.Greskell.Binder
 import Data.Greskell.GTraversal
@@ -24,7 +24,7 @@ import Data.Greskell.Greskell
 import Network.Greskell.WebSocket
 
 
--- | A `Greskell a` represents a Gremlin expression
+-- | = A `Greskell a` represents a Gremlin expression
 -- that evaluates to the type `a`.
 -- It has instances of IsString, Num, Fractional, etc.
 
@@ -35,7 +35,7 @@ literalInt :: Greskell Int
 literalInt = 200
 
 
--- | `toGremlin a` turns a `Greskell` into a text script
+-- | = `toGremlin a` turns a `Greskell` into a text script
 -- that Gremlin can evaluate.
 
 constantScriptsAreEasyToRead :: Text
@@ -43,7 +43,7 @@ constantScriptsAreEasyToRead =
   toGremlin (200 :: Greskell Int )
 
 
--- | A `Binder` can create a `Greskell` and a `Binding`.
+-- | = A `Binder` can create a `Greskell` and a `Binding`.
 
 plusTen :: Int -> Binder (Greskell Int)
 plusTen x = do
@@ -54,42 +54,27 @@ runPlusTenOnTwenty :: (Greskell Int, Binding)
 runPlusTenOnTwenty = runBinder $ plusTen 20
 
 
--- | Talk to Gremlin Server.
-
-submitScriptToGremlinServer :: IO ()
-submitScriptToGremlinServer = let
-  submitExample :: IO [Int]
-  submitExample =
-    bracket (connect "localhost" 8182) close $ \client -> do
-      let (g, binding) = runBinder $ plusTen 50
-      result_handle <- submit client g (Just binding)
-      fmap toList $ slurpResults result_handle
-
-  in do
-  egot <- try submitExample :: IO (Either SomeException [Int])
-  case egot of
-    Left _    -> putStrLn "Left"
-    Right got -> putStrLn $ "Right" ++ show got
-
-
--- | the DSL
+-- | = = The DSL
 
 findMarko :: Text
 findMarko = toGremlin $
-  (source "g" & sV [] :: GTraversal Transform ()      AVertex)
-    -- retrieves all vertices
-  &. (gHasLabel "person"    :: Walk Transform AVertex AVertex)
-  &. (gHas2 "name" "marko"  :: Walk Transform AVertex AVertex)
+  (source "g" & sV [] -- retrieves all vertices
+       :: GTraversal Transform ()      AVertex)
+  &. ( gHasLabel "person"
+       :: Walk Transform AVertex AVertex)
+  &. ( gHas2 "name" ("marko" :: Greskell String)
+       :: Walk Transform AVertex AVertex)
   -- &. and $. parallel the familiar & and $
 
 findMarko' :: Text
 findMarko' = toGremlin $
   (source "g" & sV [] :: GTraversal Transform ()      AVertex)
-  &.  (gHasLabel "person" >>> gHas2 "name" "marko")
-        -- `Walk` is an instance of `Category`
+  &. ( gHasLabel "person"
+       >>> -- `Walk` is an instance of `Category`
+       gHas2 "name" ("marko" :: Greskell String))
 
 
--- Walks can be lifted to change their WalkType (first type parameter).
+-- | = Walks can be lifted to change their WalkType (first type parameter).
 -- Filter < Transform < SideEffect
 
 hasAge   :: Walk Filter     AVertex AVertex
@@ -102,3 +87,74 @@ hasAge'' :: Walk SideEffect AVertex AVertex
 hasAge'' = liftWalk hasAge
   -- notice that a single lift can rise one or two levels
 
+
+-- | = Type safety!
+
+nameOfPeople :: Walk Filter AVertex AVertex
+             -> GTraversal Transform () Text
+nameOfPeople pfilter =
+  source "g" & sV []
+  &. gHasLabel "person"
+  &. liftWalk pfilter
+  &. gValues ["name"]
+
+getPeopleWithAgeField :: Text
+getPeopleWithAgeField = toGremlin $
+  nameOfPeople $ gHas1 "age"
+
+-- -- This does not compile, because it would pass a SideEffect walk
+-- -- to an argument that expects a Filter walk.
+-- typeError :: Text
+-- typeError = toGremlin $
+--   nameOfPeople (gAddV "person" :: Walk SideEffect s AVertex)
+
+
+-- | = Talk to Gremlin Server
+
+-- | This doesn't (I think) use the graph at all, just Gremlin.
+submitPlusTen :: IO (Either SomeException [Int])
+submitPlusTen = try submitExample where
+  submitExample :: IO [Int]
+  submitExample =
+    bracket (connect "localhost" 8182) close $ \client -> do
+      let (g, binding) = runBinder $ plusTen 50
+      result_handle <- submit client g (Just binding)
+      fmap toList $ slurpResults result_handle
+
+submitGetNamedPeople :: IO (Either SomeException [Text])
+submitGetNamedPeople = try submitExample where
+  getNameOfPeople :: Client -> IO (ResultHandle Text)
+  getNameOfPeople client =
+    submit client ( nameOfPeople
+                    gIdentity ) -- here, effectively the empty filter
+    Nothing
+
+  submitExample :: IO [Text]
+  submitExample =
+    bracket (connect "localhost" 8182) close $ \client -> do
+      result_handle <-   getNameOfPeople client
+      fmap toList $ slurpResults result_handle
+
+-- | = Sometimes you need type signatures,
+-- because the compiler can't infer start or end types
+tricky, lessTricky :: Text
+
+tricky = toGremlin gvo
+  where
+  gv :: GTraversal Transform () AVertex
+  gv = source "g" & sV []
+  gvo :: GTraversal Transform () AVertex
+  gvo = gv &. gOut []
+
+-- | But this compiles, because it uses the monomorphic `sV'`
+-- instead of `sV`. (Many polymorphic functions in Greskell.GTraversal
+-- have such partners.)
+lessTricky = toGremlin $
+  source "g" & sV' [] &. gOut' []
+
+
+-- | = Skipping: README section titled "GraphSON parser"
+-- (Also skipping: `FromGraphSON`, described in the next section,
+-- titled "Make your own graph structure types".
+
+-- | = Make your own graph structure types
