c2affa45b22314a4391afabf80c62502bbcad102
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-07-28 22:19:08 -0500

    solved: how to retrieve properties

--- src/IndexTest.hs
+++ src/IndexTest.hs
@@ -14,58 +14,55 @@ import Data.Greskell.GraphSON
 import Network.Greskell.WebSocket
 
 
--- | = Add and search for people.
+-- | = Run searches and graph changes.
 
--- It works! Try running these in various orders:
--- runSideEffect addPerson
--- runSideEffect $ liftWalk findPeople
-
+-- | To run a traversal that has no side effects,
+-- precede this operation with `liftWalk`.
 runSideEffect :: forall a. FromGraphSON a
               => GTraversal SideEffect () a
               -> IO (Either SomeException [a])
 runSideEffect script = try go where
-  go :: IO [a]
-  go =
+  go :: IO [a] =
     bracket (connect "localhost" 8182) close $ \client -> do
       result_handle <- submit client script Nothing
       fmap toList $ slurpResults result_handle
 
+
+-- | = Searches
+
 findPeople :: GTraversal Transform () AVertex
 findPeople =
-  (source "g" & sV [] :: GTraversal Transform () AVertex)
+  ( source "g" & sV [] )
   &. gHasLabel "person"
 
-findPeopleProperties =
+findPeopleProperties :: [Key AVertex d]
+  -> GTraversal Transform () (AVertexProperty d)
+findPeopleProperties props =
   ( source "g" & sV' [] )
   &. ( gHasLabel "person"
-       >>> gProperties []
-     )
+       >>> gProperties props )
 
+findPeopleValues ::
+  [Key AVertex Int] -> GTraversal Transform () Int
 findPeopleValues props =
   ( source "g" & sV' [] )
   &. ( gHasLabel "person"
-       >>> gValues props
-     )
+       >>> gValues props )
 
 
--- | = How to set propeties?
+-- | = Graph changes that involve setting properties
 
--- | I want this to add a person with "suchness" = n.
--- It adds a person, but with no properties.
+-- | add a person with "suchness" = n
 addPerson :: Int -> GTraversal SideEffect () AVertex
 addPerson n = source "g" &
-              sAddV "person"
-              &. x (gProperty "suchness" $ valueInt n)
-  -- neither of these definitions of `x` does what I want
-  where x = id
-        -- x = gSideEffect
+              sAddV "person" &.
+              gProperty "suchness" (valueInt n)
 
--- | I want this to set property "suchness" = 33
--- for every vertex in the graph. It changes nothing.
-suchness33ForEverybody :: GTraversal SideEffect () AVertex
-suchness33ForEverybody =
-  ( liftWalk ( source "g" & sV []
-               :: GTraversal Transform () AVertex )
-    :: GTraversal SideEffect () AVertex )
-  &. ( gProperty "suchness" ( 33 :: Greskell Int )
-       :: Walk SideEffect AVertex AVertex )
+-- | set "suchness" = n for every vertex
+setSuchnessForEveryVertex ::
+  Int -> Int -> GTraversal SideEffect () AVertex
+setSuchnessForEveryVertex n m =
+  liftWalk ( source "g" & sV [] )
+  &. ( ( gProperty "suchness" ( valueInt n )
+         :: Walk SideEffect AVertex AVertex )
+     >>> gProperty "essence" ( valueInt m ) )
